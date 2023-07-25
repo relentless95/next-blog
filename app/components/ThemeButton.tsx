c1bb426b9b50cf44c44e1d26e6b25813db98ff23
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-07-25 23:23:54 +0200

    feat: added sanity

--- app/components/ThemeButton.tsx
+++ app/components/ThemeButton.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 export default function Themebutton() {
   const { setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
   return (
     <button
       onClick={() => setTheme(resolvedTheme == "dark" ? "light" : "dark")}
